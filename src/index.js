d75553b0746f2a271c9b1889841d39e2ddef8010
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-10-03 13:16:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Display from './Display';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Display />
+    <Cart />
   </React.StrictMode>
 );
 
