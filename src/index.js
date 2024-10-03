f4490c2a6ca0f1c49652e2335d9449be712b3e43
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-10-03 13:20:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 
 import reportWebVitals from './reportWebVitals';
-import Display from './Display';
+import Cart from './Cart';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
